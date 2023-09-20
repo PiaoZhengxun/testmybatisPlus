0d392887229a0533084f9aa1916e00890141a61d
PiaoZhengxun <pjh951218@naver.com>
2023-09-20 22:27:21 +0800

    2023.09.18
    第五天

---
+++ src/test/java/com/neusoft/testmybatisplus/TestmybatisplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.testmybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestmybatisplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
